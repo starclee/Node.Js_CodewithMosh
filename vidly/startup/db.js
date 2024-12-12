4ec9b2bc8795baab12b1d019a9013d110e0a0cd9
starclee <sabarivijay214@gmail.com>
2024-12-12 23:39:30 +0530

    mongo Db conn str timeout issue resolved

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -1,6 +1,7 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
 const config = require("config");
+const error = require("../middleware/error");
 
 const logger = winston.createLogger({
   format: winston.format.combine(
@@ -15,5 +16,10 @@ module.exports = function () {
   let dbString = config.get("db");
   mongoose
     .connect(dbString)
-    .then(() => logger.info(`Connected to ${dbString} ...`));
+    .then(() => logger.info(`Connected to Mongo Db...`))
+    .catch((error) => {
+      logger.error(error.message);
+      logger.info("Application has been suspended... ");
+      process.exit(1);
+    });
 };
